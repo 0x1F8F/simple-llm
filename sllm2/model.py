d105e6d5a46add943fd0709653920544e018d461
0x1F8F <86254906+0x1F8F@users.noreply.github.com>
2024-08-31 12:16:15 +0530

    clean

--- sllm2/model.py
+++ sllm2/model.py
@@ -7,21 +7,18 @@ class ModelArg:
     vocab_size: int = -1
     emb_size: int = 32
     head_size: int = 64
-    layers : int = 8
-    n_heads : int = 1
     window_size: int = 20
     lr: float  = 1e-3
     batch_size: int = 32
 
 
 class Head(nn.Module):
-    def __init__(self, arg: ModelArg , hz : int):
+    def __init__(self, arg: ModelArg , inp : int):
         super().__init__()
         self.arg = arg
         self.head_size = arg.head_size
         self.bsz = arg.batch_size
-        self.hz = hz
-        emb = arg.emb_size
+        emb = inp
         self.wk = nn.Linear(emb , arg.head_size ,bias=False)
         self.wq = nn.Linear(emb , arg.head_size ,bias=False)
         self.wv = nn.Linear(emb, arg.head_size ,bias=False)
@@ -42,24 +39,20 @@ class MultiHead(nn.Module):
     def __init__(self , arg: ModelArg):
         super().__init__()
         self.arg = arg
-        # assert arg.head_size % arg.layers == 0, "cannot use this head size" + f"{arg.head_size} / {arg.layers}" 
-        self.hz = arg.head_size // arg.layers
-        # self.head_list = nn.ModuleList(list( Head(arg, self.hz) for i in range(arg.layers)))
-        self.head = Head(arg , self.hz)
     
     def forward(self , x):
         x = self.head(x)
         return(x)
-
 
 class Transformer(nn.Module):
     def __init__(self , arg: ModelArg):
         super().__init__()
         self.arg = arg
         self.embedding = nn.Embedding(arg.vocab_size , arg.emb_size) # -> (batch , token , emb)
-        self.head = MultiHead(arg)
+        self.head = Head(arg,arg.emb_size)
     
     def forward(self,x : torch.Tensor) -> torch.Tensor:
+        b , t = x.shape
         logits = self.embedding(x)
         logits = self.head(logits)
         # logits = nn.functional.softmax(logits, dim=-1)
